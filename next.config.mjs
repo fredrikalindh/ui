775d236067850764b08a0595c82d80d7f92c1be9
Fredrika Lindh <fredrika@zealy.io>
2025-10-30 11:41:09 +0100

    feat: favicon

--- next.config.mjs
+++ next.config.mjs
@@ -3,21 +3,10 @@ import { createMDX } from 'fumadocs-mdx/next';
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
-  webpack: (config, { isServer }) => {
-    // Dedupe jotai to prevent multiple instances
-    if (!isServer) {
-      config.resolve.alias = {
-        ...config.resolve.alias,
-        'jotai': require.resolve('jotai'),
-      };
-    }
-    return config;
-  },
 };
 
 const withMDX = createMDX({
   // customise the config file path
-  // configPath: "source.config.ts"
 });
 
 export default withMDX(config);
